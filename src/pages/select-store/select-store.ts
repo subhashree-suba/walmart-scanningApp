ffdf194c635ce42a9fc081182fe4747ada4043ea
subhashree-suba <suba.notes@gmail.com>
2018-10-05 19:14:46 +0530

    select-store-1

--- src/pages/select-store/select-store.ts
+++ src/pages/select-store/select-store.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { StoreapiProvider } from './../../providers/storeapi/storeapi';
 
 /**
  * Generated class for the SelectStorePage page.
@@ -16,12 +17,34 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'select-store.html',
 })
 export class SelectStorePage {
+  public store:any;
+  searchTerm: string;
+  filterItems: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public storeApiProvider: StoreapiProvider) {
+  this.getStore();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SelectStorePage');
+  }
+
+  getStore(){
+    this.storeApiProvider.getStore().subscribe(data => {
+      this.store = data;
+      // this.filterItems = this.store
+    });
+  }
+
+  filterStore(){
+    if(this.searchTerm && this.searchTerm.trim() != ''){
+    this.filterItems = this.store.filter(item =>
+      (item.store).indexOf(this.searchTerm) >-1
+    )
+    }
+    else{
+      this.filterItems = [];
+    }
   }
 
 }
--- src/providers/storeapi/storeapi.ts
+++ src/providers/storeapi/storeapi.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 /*
   Generated class for the StoreapiProvider provider.
@@ -12,6 +13,31 @@ export class StoreapiProvider {
 
   constructor(public http: HttpClient) {
     console.log('Hello StoreapiProvider Provider');
+  }
+  
+  // term: any;
+  // baseUrl: string = 'http://localhost:4001/store';
+  // queryUrl: string = '?store=';
+
+
+  getStore() {
+    return this.http.get('http://localhost:4001/store')
+    .do(this.logResponse)
+    .map(this.extractData)
+    .catch(this.catchError)
+  }
+ 
+  private catchError(error){
+    console.log(error);
+    return Observable.throw(error || "Server error.");
+  }
+ 
+  private logResponse(res) {
+    // console.log(res);
+  }
+ 
+  extractData(res){
+    return res;
   }
 
 }
